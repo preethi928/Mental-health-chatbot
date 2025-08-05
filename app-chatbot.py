e0f30cf46a32c83eb731c481983fe810552c59cb
preethi928 <67475727+preethi928@users.noreply.github.com>
2025-08-05 11:00:09 +0300

    Add files via upload

---
+++ app-chatbot.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6568cda0-2f7b-4a63-ac90-0dbb390fa74c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
